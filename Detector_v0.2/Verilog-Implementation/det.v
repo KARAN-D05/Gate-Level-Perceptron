e852ae1d9fcfd71c2d363678b1ee198dccfd9b39
Karan Diwan <karandiwan46@gmail.com>
2026-04-12 17:56:56 +0530

    EQ-SUB-SUPER-ANTI Recognition in Verilog

--- Detector_v0.2/Verilog-Implementation/det.v
+++ Detector_v0.2/Verilog-Implementation/det.v
@@ -3,14 +3,14 @@
 // ============================================================================-
 
 module sub_super_multi_pov(
-    input [15:0] in,
-    input [15:0] weight,
-    output reg sub,
-    output reg super,
-    output reg eq,
-    output reg anti,
-    output reg recognition_sub,
-    output reg recognition_super
+    input [15:0] in,              // 4*4 Input Grid
+    input [15:0] weight,          // 4*4 Weight Grid
+    output reg sub,               // Input ⊂ Weight
+    output reg super,             // Input ⊃ Weight
+    output reg eq,                // Input = Weight
+    output reg anti,              // (Input ⊄ Weight)∧(Input ⊅ Weight)∧(Input ≠ Weight)∨(Input ∩ Weight = ∅)∨(Input ∩ Weight ≠ ∅)
+    output reg recognition_sub,   // 1 if (Input ⊂ Weight)∨(Input = Weight)
+    output reg recognition_super  // 1 if (Input ⊃ Weight)∨(Input = Weight)
 );
 
   initial sub = 0;
@@ -18,13 +18,16 @@ module sub_super_multi_pov(
   initial eq = 0;
   initial anti = 0;
   
-  wire [15:0] inaw;
-  wire [15:0] inco;
-  wire [15:0] wco;
+  wire [15:0] inaw;  // in AND weights = out
+  wire [15:0] inco;  // in COMPARE out
+  wire [15:0] wco;   // weigts COMPARE out
   
-  assign inaw = in & weight;
-  assign inco = ( ((~in) & (~inaw) ) | ( in & inaw ) );
-  
+  assign inaw = in & weight; // 16 Wires carrying result of (INrc ^ WEIGHTrc), for rc=00,01,02,03,10,....,33
+    
+    assign inco = ( ((~in) & (~inaw) ) | ( in & inaw ) ); // 16 Wires carrying XNOR/Equivalence Checks for (IN XNOR inaw), for rc=00,01,02,03,.....,33
+
+
+// EQ-SUB RECOGNITION
   always@(*) begin
     if (&inco) begin 
       recognition_sub = 1;
@@ -33,8 +36,10 @@ module sub_super_multi_pov(
     end
   end
 
-  assign wco = ( ((~inaw)&(~weight)) | (inaw & weight) );
+    assign wco = ( ((~inaw)&(~weight)) | (inaw & weight) ); // 16 Wires carrying XNOR/Equivalence Checks for (weights XNOR inaw), for rc=00,01,02,03,.....,33
 
+
+//EQ-SUPER RECOGNITION
   always@(*) begin
     if (&wco) begin 
         recognition_super = 1;
@@ -50,6 +55,7 @@ module sub_super_multi_pov(
     super = 0;
     anti = 0;
 
+// 2-POV LOGICAL ANALYSIS
     if(recognition_sub & recognition_super) begin 
         eq = 1;
     end else if (~recognition_sub & recognition_super) begin
