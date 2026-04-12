1523462e721300904e38252abbc5e3b3ac545fdd
Karan Diwan <karandiwan46@gmail.com>
2026-04-12 15:55:57 +0530

    EQ-SUB Recognition in Verilog

--- Detector_v0.0/Verilog-Implementation/det.v
+++ Detector_v0.0/Verilog-Implementation/det.v
@@ -1,24 +1,20 @@
-// =============================================================================
-//  eq_sub_recognition — Gate-Level Pattern Detector (v0.0)
-// =============================================================================
-
 module eq_sub_recognition(
-    input [15:0] in,        
-    input [15:0] weight,    
-    output reg recognition  
+    input [15:0] in,        // 4*4 Input Grid     
+    input [15:0] weight,    // 4*4 Weights Grid 
+    output reg recognition  // Output (Recognition/No Recognition)
 );
 
-  wire [15:0] inaw;  
-  wire [15:0] inco;  
+  wire [15:0] inaw;        
+  wire [15:0] inco;        
 
-  assign inaw = in & weight;
+  assign inaw = in & weight;  // 16 Wires carrying result of (INrc ^ WEIGHTrc), for rc=00,01,02,03,10,....,33
 
-  assign inco = ( ((~in) & (~inaw) ) | ( in & inaw ) );
+  assign inco = ( ((~in) & (~inaw) ) | ( in & inaw ) ); // 16 Wires carrying XNOR/Equivalence Checks for (IN XNOR inaw), for rc=00,01,02,03,.....,33
 
   always@(*) begin
-    if (&inco) begin      
+    if (&inco) begin  // If all the pixels between in and inaw match we get recognition 
       recognition = 1;
-    end else begin
+    end else begin  
       recognition = 0;
     end
   end
