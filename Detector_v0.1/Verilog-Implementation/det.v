4bf99f9937f66fd28e244853ab310c899fd4cfd2
Karan Diwan <karandiwan46@gmail.com>
2026-04-12 17:23:24 +0530

    EQ-SUPER Recognition in Verilog

--- Detector_v0.1/Verilog-Implementation/det.v
+++ Detector_v0.1/Verilog-Implementation/det.v
@@ -3,19 +3,19 @@
 // ============================================================================
 
 module eq_super_recognition(
-    input [15:0] in,
-    input [15:0] weight,
-    output reg recognition
+    input [15:0] in,         // 4*4 Input Grid
+    input [15:0] weight,     // 4*4 Weights Grid
+    output reg recognition   // Output (Recognition/No Recognition)
 );
 
-  wire [15:0] inaw;
-  wire [15:0] wco;
+  wire [15:0] inaw;       // (in AND weights) = out
+  wire [15:0] wco;        // weights COMPARE out
 
-  assign inaw = in & weight;
-  assign wco = ( ((~inaw)&(~weight)) | (inaw & weight) );
+  assign inaw = in & weight;   // 16 Wires carrying result of (INrc ^ WEIGHTrc), for rc=00,01,02,03,10,....,33
+  assign wco = ( ((~inaw) & (~weight)) | (inaw & weight) );   // 16 Wires carrying XNOR/Equivalence Checks for (weights XNOR inaw), for rc=00,01,02,03,.....,33
 
   always@(*) begin
-    if (&wco) begin 
+    if (&wco) begin // If all the pixels between in and inaw match we get recognition 
         recognition = 1;
     end else begin
         recognition = 0;
