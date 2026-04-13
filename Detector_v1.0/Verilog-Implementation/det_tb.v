933dcf2d1b5b62f5edb1b6356cdd25b0169ed85d
Karan Diwan <karandiwan46@gmail.com>
2026-04-13 16:53:24 +0530

    Manually Alterable Decision Boundary - Verilog

--- Detector_v1.0/Verilog-Implementation/det_tb.v
+++ Detector_v1.0/Verilog-Implementation/det_tb.v
@@ -20,31 +20,38 @@ module testbench;
       $dumpfile("sim.vcd");
       $dumpvars(0, testbench);
 
+      // M=8, θ=12 , θ > M -> Recognition=0
       in = 16'hE44E;
       ref = 16'h7227;
       thresh = 4'hC;
       #10;
 
+      // Same test case with different threshold
+      // M=8, θ=7 , θ < M -> Recognition=1
       in = 16'hE44E;
       ref = 16'h7227;
       thresh = 4'h7;
       #10;
 
+      // M=0, θ=0(MIN) , θ = M -> Recognition=0
       in = 16'h0000;
       ref = 16'hFFFF;
       thresh = 4'h0;
       #10;
 
+     // M=16, θ=15(MAX) , θ < M -> Recognition=1
       in = 16'hFFFF;
       ref = 16'hFFFF;
       thresh = 4'hF;
       #10;
 
+      // M=15, θ=13 , θ < M -> Recognition=1
       in = 16'hE444;
       ref = 16'hF444;
       thresh = 4'hD;
       #10;
 
+      // M=15, θ=14 , θ < M -> Recognition=1
       in = 16'hE444;
       ref = 16'hF444;
       thresh = 4'hE;
